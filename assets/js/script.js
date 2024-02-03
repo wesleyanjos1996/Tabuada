7daecc2f69f4083f97f563d004049094101cd574
WESLEY DOS ANJOS <wesleyanjosprogramming@outlook.com.br>
2024-02-03 16:56:26 -0300

    End project Tabuada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,27 @@
-let tab = document.querySelector('#tab tbody')
-let valueA = 15
-tab.innerHTML = ''
+function calcTab() 
+{
 
-for (let valueB = 0; valueB < 11; valueB++) {
-    let result = valueA * valueB
-    let template = `
+    let tab = document.querySelector('#tab tbody')
+    let valueA = parseInt(document.querySelector('#valueA').value)
+
+    tab.innerHTML = ''
+
+    for (let valueB = 0; valueB < 11; valueB++) {
+        let result = valueA * valueB
+        let template = `
         <td>${valueA}</td>
         <td>X</td>
         <td>${valueB}</td>
         <td>=</td>
         <td>${result}</td>
      `
-    let tr = document.createElement('tr')
-    tr.innerHTML = template
-    tab.append(tr)
+        let tr = document.createElement('tr')
+        tr.innerHTML = template
+        tab.append(tr)
+    }
+
 }
+
+calcTab()
+
+document.querySelector('#valueA').addEventListener('change', calcTab)
